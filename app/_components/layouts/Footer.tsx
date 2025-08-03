8b9fa88a5ea9927cda2575a5c79c335ff21b9888
dzunght95 <dzunght95@gmail.com>
2025-08-03 11:16:29 +0700

    fix: update version fetching to use a dynamic timestamp

--- app/_components/layouts/Footer.tsx
+++ app/_components/layouts/Footer.tsx
@@ -40,7 +40,7 @@ const Footer: React.FC = () => {
   const [versionText, setVersionText] = useState<string>('loading...');
 
   useEffect(() => {
-    fetch(`/version.json?v=1.1`)
+    fetch(`/version.json?v=${new Date().getTime()}`)
       .then((res) => res.json())
       .then((data) => {
         const { major, minor, buildDate, buildTime } = data;
